1cff13d5380e638a839ada7f67186742813a196b
toshi0383 <t.suzuki326@gmail.com>
2016-01-22 15:04:38 +0900

    Initial

---
+++ sample-tvos-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample-tvos-app
+//
+//  Created by toshi0383 on 1/22/16.
+//  Copyright © 2016 nextscape. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
